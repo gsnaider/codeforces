adc8ca15d0efe0be61c3547eaa18c76fd9b9623b
Gaston Snaider <gastonsnaider93@gmail.com>
2017-08-26 02:05:09 -0300

    Problem 4C solved

---
+++ codeforces/src/main/java/gs/codeforces/problems/problem4c/Problem4CSolver.java
@@ -0,0 +1,40 @@
+package gs.codeforces.problems.problem4c;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Problem4CSolver {
+
+	public static void main(String[] args) {
+		
+		Set<String> database = new HashSet<>();
+		Map<String, Integer> nextNumbers = new HashMap<>();
+		
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		for (int i = 0; i < n; i++) {
+			String name = scanner.nextLine();
+			if (database.add(name)) {
+				System.out.println("OK");
+			} else {
+				int nextNumber;
+				if (nextNumbers.containsKey(name)) {
+					nextNumber = nextNumbers.get(name) + 1;
+				} else {
+					nextNumber = 1;
+				}
+				nextNumbers.put(name, nextNumber);
+				String newName = name + nextNumber;
+				database.add(newName);
+				System.out.println(newName);
+			}
+		}
+		scanner.close();
+		
+	}
+	
+}
